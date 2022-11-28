4550aa0631a23f948095ffe02cfd71a2e2e08fe0
alvinleegw <alvin-leegw@hotmail.com>
2022-11-28 10:35:20 +0800

    Add Login

--- EMS/Login.Designer.cs
+++ EMS/Login.Designer.cs
@@ -28,19 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.signinButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.errorLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.loginLabel.Location = new System.Drawing.Point(486, 54);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(155, 65);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.usernameLabel.Location = new System.Drawing.Point(160, 154);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(198, 48);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.passwordLabel.Location = new System.Drawing.Point(169, 309);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(189, 48);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(364, 154);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(457, 55);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(364, 309);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(457, 55);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // signinButton
+            // 
+            this.signinButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.signinButton.Location = new System.Drawing.Point(477, 410);
+            this.signinButton.Name = "signinButton";
+            this.signinButton.Size = new System.Drawing.Size(164, 69);
+            this.signinButton.TabIndex = 5;
+            this.signinButton.Text = "Sign In";
+            this.signinButton.UseVisualStyleBackColor = true;
+            this.signinButton.Click += new System.EventHandler(this.signinButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.resetButton.Location = new System.Drawing.Point(657, 410);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(164, 69);
+            this.resetButton.TabIndex = 6;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(186, 230);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 48);
+            this.errorLabel.TabIndex = 7;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 48F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1108, 738);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.signinButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Name = "Login";
+            this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label loginLabel;
+        private Label usernameLabel;
+        private Label passwordLabel;
+        private TextBox usernameTextBox;
+        private TextBox passwordTextBox;
+        private Button signinButton;
+        private Button resetButton;
+        private Label errorLabel;
     }
 }
--- EMS/Login.cs
+++ EMS/Login.cs
@@ -1,5 +1,6 @@
 using System.Data;
-using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
 using MySql.Data.MySqlClient;
 
 namespace EMS
@@ -9,20 +10,90 @@ namespace EMS
         public Login()
         {
             InitializeComponent();
-            /*string connectionString = null;
-            MySqlConnection cnn;
-            connectionString = "server=localhost;database=ems;uid=root;pwd=;";
-            cnn = new MySqlConnection(connectionString);
-            try
+        }
+
+        private void signinButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+            string hash;
+            string pass;
+            if (username == "" || password == "")
             {
-                cnn.Open();
-                MessageBox.Show("Connection Open !");
-                cnn.Close();
+                errorLabel.Text = "Please enter username and password.";
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show("Can not open connection ! ");
-            }*/
+                using (var md5Hash = MD5.Create())
+                {
+                    var sourceBytes = Encoding.UTF8.GetBytes(password);
+                    var hashBytes = md5Hash.ComputeHash(sourceBytes);
+                    hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+                }
+                string connectionString = null;
+                MySqlConnection connection;
+                connectionString = "server=localhost;database=ems;uid=root;pwd=;";
+                connection = new MySqlConnection(connectionString);
+                if (username == "admin")
+                {
+                    try
+                    {
+                        connection.Open();
+                        MySqlCommand command = connection.CreateCommand();
+                        command.CommandText = "SELECT password from ADMIN";
+                        pass = command.ExecuteScalar().ToString();
+                        if (hash == pass)
+                        {
+                            MessageBox.Show("Welcome Back Admin");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid Password");
+                        }
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Can not open connection!");
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        connection.Open();
+                        MySqlCommand command = connection.CreateCommand();
+                        command.CommandText = "SELECT password from EMPLOYEE WHERE username='" + username + "'";
+                        if(command.ExecuteScalar() != null)
+                        {
+                            pass = command.ExecuteScalar().ToString();
+                            if (hash == pass)
+                            {
+                                errorLabel.ForeColor = Color.Green;
+                                errorLabel.Text = "Successfully Login.";
+                                MessageBox.Show("Welcome back " + username);
+                            }
+                            else
+                            {
+                                errorLabel.Text = "Invalid Password";
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Possible Empty Table or Connection Error.");
+                    }
+                }
+                
+            }
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            usernameTextBox.Text = "";
+            passwordTextBox.Text = "";
+            errorLabel.Text = "";
         }
     }
 }
